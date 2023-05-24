3a0837abe3c43008ea7c387fed1a2b1f1ec775ef
“admin” <2768800845@qq.com>
2023-05-24 15:35:14 +0800

    first commit 2

--- src/main/java/com/xuexiao/git/testGit.java
+++ src/main/java/com/xuexiao/git/testGit.java
@@ -3,5 +3,6 @@ package com.xuexiao.git;
 public class testGit {
     public static void main(String[] args) {
         System.out.println("hello.git");
+        System.out.println("hello.git2");
     }
 }
